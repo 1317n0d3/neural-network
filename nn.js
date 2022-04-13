db7f3154354c3e2ea251af9d08e5fa577da79f78
klltx <kostya.svetashov.01@gmail.com>
2022-04-13 11:51:17 +0400

    update project structure

---
+++ nn.js
@@ -0,0 +1,8 @@
+const mmap = math.map; // to be used to pass each element of a matrix to a function
+const rand = math.random;
+const transp = math.transpose;
+const mat = math.matrix;
+const e = math.evaluate;
+const sub = math.subtract;
+const sqr = math.square;
+const sum = math.sum;
